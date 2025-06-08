04eceb1babfc722b64a69b5bf6aedd69280a37e1
Joshua Salceda <joshuasalceda0021@gmail.com>
2025-06-08 19:20:33 +0800

    changes

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,11 +39,26 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => storage_path('api/storage'),
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
+
+        // 'public' => [
+        //     'driver' => 'local',
+        //     'root' => storage_path('app/public'),
+        //     'url' => env('APP_URL').'/storage',
+        //     'visibility' => 'public',
+        //     'throw' => false,
+        // ],
+
+        // 'api_public' => [
+        //     'driver' => 'local',
+        //     'root' => base_path('api/storage'),
+        //     'url' => env('APP_URL') . '/storage',
+        //     'visibility' => 'public',
+        // ],
 
         's3' => [
             'driver' => 's3',
